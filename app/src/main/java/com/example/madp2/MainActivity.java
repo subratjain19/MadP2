ae44a426ffe60ef50838bebdf2d395d39e4e6a58
swasti2002 <201b281@juetguna.in>
2022-05-28 18:48:48 +0530

    commited by swasti

--- app/src/main/java/com/example/madp2/MainActivity.java
+++ app/src/main/java/com/example/madp2/MainActivity.java
@@ -8,18 +8,33 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isWinner=false;
+    int imageClick=-1;
     int player=1;
+    int [][]winningStates={{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6},{0,3,6},{1,4,7},{2,5,8}};
+    int []gameState={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
     public void load(View view){
         ImageView v=(ImageView)view;
         int tag=Integer.parseInt(v.getTag().toString());
         if(player==1){
             v.setImageResource(R.drawable.t);
+            gameState[tag]=player;
             Toast.makeText(this,tag+""+"Tom",Toast.LENGTH_SHORT).show();
             player=0;
         }else{
             v.setImageResource(R.drawable.j);
+            gameState[tag]=player;
             Toast.makeText(this,tag+""+"Jerry",Toast.LENGTH_SHORT).show();
             player=1;
+        }
+
+        for(int i=0;i<winningStates.length;i++){
+            if(gameState[winningStates[i][0]]==gameState[winningStates[i][1]] &&
+                    gameState[winningStates[i][1]]==gameState[winningStates[i][2]]
+                    && gameState[winningStates[i][0]]>-1){
+                Toast.makeText(this,"Winner is "+(player==0?"Tom":"Jerry"),Toast.LENGTH_SHORT).show();
+            }
         }
     }
     @Override
